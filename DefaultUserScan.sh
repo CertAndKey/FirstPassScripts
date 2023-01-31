41cad21c2ae61d5f07e26c5b83e6435006c04d9b
CertAndKey <38504609+CertAndKey@users.noreply.github.com>
2023-01-31 08:01:53 -0500

    Update DefaultUserScan.sh
    added check for passwd file

--- DefaultUserScan.sh
+++ DefaultUserScan.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#RUN THIS SCRIPT FROM THE "/" DIRECTORY OF A DEVICE
+
 On_Red='\033[41m'
 On_Green='\033[42m'
 Color_Off='\033[0m'
@@ -7,7 +9,14 @@ BWhite='\033[1;37m'
 
 printf ${BWhite}
 
-cat /etc/passwd 2>/dev/null | grep -i "sh$" | cut -d ":" -f 1 >> tmp.txt
+if ! [[ -f etc/passwd ]]
+then
+  printf "Couldn't find etc/passwd\n"
+  printf "${On_Red}FAILED${Color_Off}\n"
+  exit 1
+fi
+
+cat etc/passwd 2>/dev/null | grep -i "sh$" | cut -d ":" -f 1 >> tmp.txt
 
 user1=$(echo -n -e '\x72\x6f\x6f\x74\x0a')
 user2=$(echo -n -e '\x76\x69\x65\x77\x65\x72\x75\x73\x65\x72\x0a')
@@ -30,3 +39,4 @@ else
 fi
 
 rm tmp.txt
+
